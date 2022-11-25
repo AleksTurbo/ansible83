0fed0d14180c8259f958be1944a9aca76121f4c8
AleksTurbo <aleksturbo@yandex.ru>
2022-11-25 22:07:02 +0300

    trfm

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+provider "yandex" {
+  service_account_key_file = "key.json"
+  zone                     = "ru-central1-a"
+  cloud_id                 = "${var.yandex_cloud_id}"
+  folder_id                = "${var.yandex_folder_id}"
+
+}
+
+resource "yandex_iam_service_account" "sa" {
+  name = "aleksturbo"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = "${var.yandex_folder_id}"
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "netology"
+}
